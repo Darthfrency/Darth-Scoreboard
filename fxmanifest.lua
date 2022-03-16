caa97354691a091470b8e7474f8693379cfea2f8
Darthfrency <francesco.cortigiano07@gmail.com>
2022-03-16 04:48:42 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+-- Generated automaticly by RB Generator.
+fx_version('cerulean')
+games({ 'gta5' })
+
+
+server_scripts({
+    "server.lua"
+});
+
+client_scripts({
+    "nui.lua"
+});
+ui_page "html/index.html"
+files {
+    'html/index.html',
+    'html/index.js',
+    'html/index.css',
+    'html/reset.css'
+}
